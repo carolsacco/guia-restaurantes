b2d2e2750bdaa85bd6a700c2abf7a553e1e29bf7
31420397 <31420397@087847-P33S105.macklabs.br>
2014-09-04 12:22:16 -0300

    5.2 teste e operacoes

---
+++ operacoes.php
@@ -0,0 +1,78 @@
+<?php
+
+function soma($x, $y) {
+            $soma = $x+$y;
+                
+    return $soma;
+    
+}
+
+$resultado1=6;
+$resultado2=2;
+
+echo "O resultado eh: " .soma($resultado1, $resultado2);
+
+echo"<br>
+    <br>
+    <br>"
+
+?>
+ 
+
+<?php 
+
+function subtracao($x, $y) {
+		$subtracao = $x-$y;
+                
+        return $subtracao;
+        
+}     
+
+
+echo "O resultado eh: " .subtracao($resultado1, $resultado2);
+                
+echo"<br>
+    <br>
+    <br>"
+?>
+
+
+
+<?php
+
+function multiplicacao($x,$y) {
+		$multiplicacao = $x*$y;
+                
+        return $multiplicacao;
+        
+}            
+
+
+echo "O resultado eh: " .multiplicacao($resultado1, $resultado2);
+                
+echo"<br>
+    <br>
+    <br>"
+?>
+
+
+<?php 
+
+function divisao($x,$y) {
+        $divisao=$x/$y;
+    
+                
+        return $divisao;
+        
+}            
+
+
+echo "O resultado eh: " .divisao($resultado1, $resultado2);
+
+echo"<br>
+    <br>
+    <br>"
+
+?>
+
+
---
+++ teste.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    
+    <body>
+        <?php include "operacoes.php"; 
+            
+        ?>
+    </body>
+</html>
